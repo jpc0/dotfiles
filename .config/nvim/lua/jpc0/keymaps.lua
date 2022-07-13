c16610ae949caa6c9ccfd2233ab3fae6277602e4
Jean-Pierre Coetzee <jeanpierrec19@gmail.com>
2022-07-13 23:43:31 +0200

    Added telescope to nvim

--- .config/nvim/lua/jpc0/keymaps.lua
+++ .config/nvim/lua/jpc0/keymaps.lua
@@ -1,1 +1,5 @@
 vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>tf", "<cmd>Telescope find_files<cr>", {remap = false})
+vim.keymap.set("n", "<leader>tgf", "<cmd>Telescope git_files<cr>", {remap = false})
+vim.keymap.set("n", "<leader>tg", "<cmd>Telescope live_grep<cr>", {remap = false})
--- .config/nvim/lua/jpc0/plugins.lua
+++ .config/nvim/lua/jpc0/plugins.lua
@@ -37,6 +37,7 @@ return packer.startup(function(use)
 	use "nvim-lua/plenary.nvim"
 	use "nvim-lua/popup.nvim"
 	-- Telescope
+	use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 	use "nvim-telescope/telescope.nvim"
 	-- Colorscheme
 	use "folke/tokyonight.nvim"
