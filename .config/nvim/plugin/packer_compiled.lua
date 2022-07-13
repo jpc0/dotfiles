aeeafaa1d5dfd6e93fd8d318b0d8fef9c1cc92bf
Jean-Pierre Coetzee <jeanpierrec19@gmail.com>
2022-07-13 19:42:39 +0200

    Fixed type in nvim plugin config

--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -44,8 +44,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/jean-pierrecoetzee/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/jean-pierrecoetzee/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/jean-pierrecoetzee/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/jean-pierrecoetzee/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/jean-pierrecoetzee/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/home/jpc0/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/jpc0/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/jpc0/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/jpc0/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/jpc0/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -71,22 +71,22 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/Users/jean-pierrecoetzee/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/home/jpc0/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/Users/jean-pierrecoetzee/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/home/jpc0/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/Users/jean-pierrecoetzee/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/home/jpc0/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["tokyonight.nvim"] = {
     loaded = true,
-    path = "/Users/jean-pierrecoetzee/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
+    path = "/home/jpc0/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
     url = "https://github.com/folke/tokyonight.nvim"
   }
 }
