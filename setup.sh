0bbcdfbc1cfa08697333845cab27885cea8b56c4
Jean-Pierre Coetzee <jeanpierrec19@gmail.com>
2023-03-16 12:54:51 +0200

    Fixed build tools in setup script and added LSP install

--- setup.sh
+++ setup.sh
@@ -3,11 +3,9 @@
 set -xe
 
 sudo apt update && sudo apt upgrade -y
-sudo apt install tmux build-essential cmake ninja-build zsh -y
-
+sudo apt install tmux build-essential cmake ninja-build zsh python3-pip -y
 
 # Setup shell
-
 curl -sS https://starship.rs/install.sh | sh
 RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -31,8 +29,51 @@ ln -s $HOME/git/dotfiles/.zshrc-e $HOME/.zshrc-e
 mkdir -p $HOME/.config
 ln -s $HOME/git/dotfiles/.config/nvim $HOME/.config/nvim
 
+mkdir -p $HOME/devel/git/work
+mkdir -p $HOME/bin
+
+cp update-nvim.sh $HOME/bin/update-nvim
+
 # Setup dev tools
+
+# Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-curl --proto '=https' -sS https://github.com/bazelbuild/bazelisk/releases/download/v1.16.0/bazelisk-linux-amd64 > bazelisk
-sudo mv bazelisk /usr/local/bin/bazelisk
+# Bazelisk
+wget https://github.com/bazelbuild/bazelisk/releases/download/v1.16.0/bazelisk-linux-amd64
+sudo mv bazelisk-linux-amd64 /usr/local/bin/bazelisk
+sudo chmod +x /usr/local/bin/bazelisk
+sudo ln -s /usr/local/bin/bazelisk /usr/local/bin/bazel
+
+# Node
+curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&\
+sudo apt-get install -y nodejs
+
+# Language Servers
+
+# Emmet
+sudo npm install -g emmet-ls
+# Docker
+sudo npm install -g dockerfile-language-server-nodejs
+# Tailwind
+sudo npm install -g @tailwindcss/language-server
+# Typescript
+sudo npm install -g typescript typescript-language-server
+# CSS and JSON
+sudo npm install -g vscode-langservers-extracted
+# C/C++
+sudo apt install clangd -y
+# CMAKE
+pip install cmake-language-server
+# Python
+pip install pyright
+# Lua
+mkdir -p $HOME/bin/lua-language-server-data
+cd $HOME/bin/lua-language-server-data
+wget https://github.com/LuaLS/lua-language-server/releases/download/3.6.17/lua-language-server-3.6.17-linux-x64.tar.gz -O - | tar -xz
+cd $HOME/bin
+touch lua-language-server
+echo "#!/bin/bash" > lua-language-server
+echo "exec \"$HOME/bin/lua-language-server-data/bin/lua-language-server\" \"$@\"" >> lua-language-server
+chmod +x lua-language-server
+
