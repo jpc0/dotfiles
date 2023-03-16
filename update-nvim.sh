c590817a50140618ea8fe133a2ebc5fc8f0d1496
Jean-Pierre Coetzee <jeanpierrec19@gmail.com>
2023-03-16 12:55:06 +0200

    Fixed update neovim script

--- update-nvim.sh
+++ update-nvim.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-sudo apt-get install ninja-build gettext libtool-bin cmake g++ pkg-config unzip curl
-cd $HOME/git/neovim
-git fetch && git pull
-make CMAKE_BUILD_TYPE=RelWithDebInfo
+set -xe
+
+cd $HOME/git
+rm -rf neovim
+git clone https://github.com/neovim/neovim -b release-0.8
+cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
